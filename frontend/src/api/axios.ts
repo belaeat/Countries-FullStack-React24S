98f3c0f4c6949e5c1fafd9399a84a15ed23e1075
belaeat <omyhossain@gmail.com>
2025-05-22 15:17:39 +0300

    update: variables

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,9 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: import.meta.env.PROD
-    ? 'https://countries-fullstack-react24s.onrender.com'
-    : 'http://localhost:5001',
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     'Content-Type': 'application/json',
   },
